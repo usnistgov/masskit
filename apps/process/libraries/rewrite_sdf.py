b8625ff5e6469d4f2b717a9c5f353ec3290feb04
lyg <lewis.geer@nist.gov>
2023-01-28 22:06:42 -0500

    changes for 2020 ri training

--- apps/process/libraries/rewrite_sdf.py
+++ apps/process/libraries/rewrite_sdf.py
@@ -7,7 +7,7 @@ to create sdf files from nist libraries, use commands like
 
 wine lib2nist64.exe /log9 hr_msms_nist2020_v47.log /OutSDF /ToV2000 z:\\home\\lyg\\data\\nist\\2020\\v47\\hr_msms_nist2020_v42 =hr_msms_nist2020_v47.orig.sdf
 # get rid of non unicode characters in chemical names.
-iconv -f utf-8 -t utf-8 -c ~/nist/hr_msms_nist2020_v47.orig.sdf > ~/nist/hr_msms_nist2020_v42.sdf
+iconv -f utf-8 -t utf-8 -c ~/nist/hr_msms_nist2020_v47.orig.sdf | uniq > ~/nist/hr_msms_nist2020_v42.sdf
 """
 
 parser = argparse.ArgumentParser()
