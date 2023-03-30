9a897cc00bdff7632eba4eaf1a18c06e7cd749db
lyg <lewis.geer@nist.gov>
2023-03-29 21:21:52 -0400

    - add default sites to unimod - delete previous default site info - redo spectrum generator to use new mod format - break out modification code - create string format for modications - replace modification code in fasta2peptides - add fixed mods to fasta2peptides

--- tests/masskit/peptide/test_spectrum_generator.py
+++ tests/masskit/peptide/test_spectrum_generator.py
@@ -5,7 +5,9 @@ import numpy as np
 
 def test_generate_peptide_library():
     df = msps.generate_peptide_library(num=2, min_length=5, max_length=30, min_charge=1, max_charge=8, min_ev=10,
-                                    max_ev=60, mod_list=['Acetyl', 'Phospho'])
+                                    max_ev=60, mod_list='Acetyl,Phospho{S/T0}',
+                                    mod_probability=1.0)
+    assert 0 in df['mod_positions'].iloc[0] and 1 in df['mod_names'].iloc[0]
     
 def test_create_peptide_name():
     output = msps.create_peptide_name("AAS", 3, ['Acetyl', 'Phospho'], np.array([1,2]), 50)
